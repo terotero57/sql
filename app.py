691771a5952ea3a9a3550f1682fe0241c763adb1
LEE YIH SHU <terotero57@gmail.com>
2022-03-09 21:04:48 +0800

    SQL記入エリア作成

--- app.py
+++ app.py
@@ -10,6 +10,19 @@ def main():
     if choice == "Home":
         st.subheader("Homepage")
 
+        col1, col2 = st.columns(2)
+
+        with col1:
+            with st.form(key='query_form'):
+                raw_code = st.text_area("SQL Code Here")
+                submit_code = st.form_submit_button("Execute")
+
+            with col2:
+                if submit_code:
+                    st.info("Query submitted")
+                    st.write(raw_code)
+
+
     else:
         st.subheader("About")
 
