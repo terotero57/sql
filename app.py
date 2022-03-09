91d3021153bd50af014c4b2ddb93f073a0d53b07
LEE YIH SHU <terotero57@gmail.com>
2022-03-09 20:58:57 +0800

    app.py初めての作成

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+
+def main():
+    st.title("SQLPlayground")
+
+    menu = ["Home", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+    if choice == "Home":
+        st.subheader("Homepage")
+
+    else:
+        st.subheader("About")
+
+
+if __name__ == "main":
+    main()
